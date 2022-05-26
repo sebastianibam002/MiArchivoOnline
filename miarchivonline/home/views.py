28be2bb5515ba3ebe7da7fdf2d6885b350712fad
sebastianibam002 <sebastianibam@gmail.com>
2022-05-25 21:20:42 -0400

    creation of the view

--- miarchivonline/home/views.py
+++ miarchivonline/home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
+
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "home/home.html")
